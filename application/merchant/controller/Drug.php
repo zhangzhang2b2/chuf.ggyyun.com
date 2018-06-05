69152a0c34ad036addc2e4a125fd1162f61993e0
langyuanzhang <982466450@qq.com>
2018-06-05 12:05:45 +0800

    药品导入excel

--- application/merchant/controller/Drug.php
+++ application/merchant/controller/Drug.php
@@ -99,5 +99,85 @@ class Drug extends Main
         $this->success('删除成功');
     }
 
+    //excel导入数据库
+    public function excle_drug()
+    {
+        if (Request::instance()->isPost()) {
+            $post = $this->request->post();
+            $this->success('登记成功');
+
+        } else {
+            return  $this->fetch();
+        }
+    }
+
+    //上传文件
+    public function do_upload(){
+
+        //引入文件
+        \think\Loader::import('PHPExcel.PHPExcel');
+        $objPHPExcel = new \PHPExcel();
+
+        //获取表单上传文件
+        $file = request()->file('file');
+        $info = $file->validate(['ext' => 'xlsx,xls'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+        //数据为空返回错误
+        if(empty($info)){
+            $output['status'] = false;
+            $output['info'] = '导入数据失败~';
+            $this->ajaxReturn($output);
+        }
+
+        //获取文件名
+        $exclePath = $info->getSaveName();
+        //上传文件的地址
+        $filename = ROOT_PATH . 'public' . DS . 'uploads'.DS . $exclePath;
+
+
+        $extension = strtolower( pathinfo($filename, PATHINFO_EXTENSION) );
+
+        \think\Loader::import('PHPExcel.IOFactory.PHPExcel_IOFactory');
+        if ($extension =='xlsx') {
+            $objReader = new \PHPExcel_Reader_Excel2007();
+            $objExcel = $objReader ->load($filename);
+
+        } else if ($extension =='xls') {
+
+            $objReader = new \PHPExcel_Reader_Excel5();
+            $objExcel = $objReader->load($filename);
+
+
+        }
+
+
+        $excel_array=$objExcel->getsheet(0)->toArray();   //转换为数组格式
+        array_shift($excel_array);  //删除第一个数组(标题);
+        array_shift($excel_array);  //删除th
+
+        $data=[];
+        foreach ($excel_array as $k=>$v){
+            $data[$k]["danhao"]=$v[0];//单号
+            $data[$k]["type_name"]=$v[1];//类型名称
+            $data[$k]["name"]=$v[2];
+            $data[$k]["number"]=$v[3];
+            $data[$k]["price"]=$v[4];
+            $data[$k]["danwei"]=$v[0];
+            $data[$k]["create_user"]=$v[5];
+            $data[$k]["create_time"]=$v[6];
+            $data[$k]["remark"]=$v[7];
+        }
+
+        $msg=[
+            'code'=>1,
+            'msg'=>'已获取信息',
+        ];
+        $msg['data']['src']=$filename;
+        $msg['data']['data']=$data;
+
+        return json_encode($msg);
+
+
+    }
 
 }
