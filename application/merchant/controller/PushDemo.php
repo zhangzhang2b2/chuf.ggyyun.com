f28be9e61fddc75cbe49e90598debb0dc25fed38
langyuanzhang <982466450@qq.com>
2018-06-05 08:59:47 +0800

    thinkphp5结合workerman的消息推送实例_基于web-msg-sender进行消息推送

--- application/merchant/controller/PushDemo.php
+++ application/merchant/controller/PushDemo.php
@@ -1,8 +1,7 @@
 <?php
 namespace app\merchant\controller;
 
-
-use app\lib\event\PushEvent;
+use app\lib\event\PushEvent;    //引用文件
 
 /**
  * 推送demo
@@ -20,7 +19,7 @@ class PushDemo
         $string = 'Man Always dddddd';  //推送的消息内容
         // $string = input('msg') ? : $string;  //判断是否有输入消息
         $push = new PushEvent();    //实例化对象
-        $push->setUser('123')->setContent($string)->push();   //setUser指定推送给的用户，setContent推送的内容
+        $push->setUser('123')->setContent($string)->push();   //setUser指定推送给的用户123，设置空则全部用户都推送。setContent推送的内容
 
     }
 
